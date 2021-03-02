ce7babc4a0609229a5ff6c59fe92ffdc4aab23f9
Brian Hopkinson <bmhopkin@uga.edu>
2021-03-02 09:53:18 -0500

    add save-load options to templated vocab

---
+++ build/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/BowVector.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/BowVector.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/FBrief.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/FBrief.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/FORB.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/FORB.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/FeatureVector.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/FeatureVector.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/QueryResults.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/QueryResults.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/Random.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/Random.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/ScoringObject.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/ScoringObject.cpp.o"
+  "/home/cv-bhlab/Documents/Libraries/DBoW2/src/Timestamp.cpp" "/home/cv-bhlab/Documents/Libraries/DBoW2/build/CMakeFiles/DBoW2.dir/src/Timestamp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBoW2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/DBoW2"
+  "../include"
+  "/home/cv-bhlab/Documents/Libraries/opencv/opencv_3_4/install/include"
+  "/home/cv-bhlab/Documents/Libraries/opencv/opencv_3_4/install/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
